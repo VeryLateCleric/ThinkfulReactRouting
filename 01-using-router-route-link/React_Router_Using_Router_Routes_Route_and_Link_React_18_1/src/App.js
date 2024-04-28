0b3395752abf1acffed60cbf1be2baff0101447c
Kenneth <kennethyoung.2960@gmail.com>
2024-04-28 13:44:11 -0700

    Corrected headings to match test

--- 01-using-router-route-link/React_Router_Using_Router_Routes_Route_and_Link_React_18_1/src/App.js
+++ 01-using-router-route-link/React_Router_Using_Router_Routes_Route_and_Link_React_18_1/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 
-const Home = () => <h1>Home Page</h1>;
-const About = () => <h1>About Page</h1>;
-const Contact = () => <h1>Contact Page</h1>;
-const NotFound = () => <h1>404 - Not Found</h1>;
+const Home = () => <h1>Welcome to the home page</h1>;
+const About = () => <h1>You are on the about page</h1>;
+const Contact = () => <h1>Please feel free to email us</h1>;
+const NotFound = () => <h1>404 Not Found</h1>;
 
 function App() {
   return (
