fc3adbaa585dd74f96a9d4b95c5443886ecee482
Kenneth <kennethyoung.2960@gmail.com>
2024-04-28 13:41:07 -0700

    Corrected Switch to Routes with updated documentation and links linking

--- 01-using-router-route-link/React_Router_Using_Router_Routes_Route_and_Link_React_18_1/src/App.js
+++ 01-using-router-route-link/React_Router_Using_Router_Routes_Route_and_Link_React_18_1/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
+
+const Home = () => <h1>Home Page</h1>;
+const About = () => <h1>About Page</h1>;
+const Contact = () => <h1>Contact Page</h1>;
+const NotFound = () => <h1>404 - Not Found</h1>;
 
 function App() {
   return (
     // No need to add <Router>, it has been added to ./index.js
     <div className="App">
+      <nav>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/contact">Contact</Link>
+      </nav>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
